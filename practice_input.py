2b60522896aed84893f977c1eb4127f37213f1d1
Christopher Cajamarca <cac10199@nyu.edu>
2024-05-28 12:54:54 -0400

    done

--- practice_input.py
+++ practice_input.py
@@ -10,7 +10,8 @@ def get_favorite_vegetable():
     and then prints out, "Interesting! I also love X!",
     where X is replaced with the user's favorite vegetable.
     """
-    # write your code here.
+    favorite_vegetable = input("What is your favorite vegetable? ")
+    print('Interesting! I also love ' + favorite_vegetable + '!')
 
 
 def get_favorite_number():
@@ -19,8 +20,8 @@ def get_favorite_number():
     and then prints out, "Interesting! I also love X!",
     where X is replaced with the user's favorite number.
     """
-    # write your code here.
-
+    favorite_number = input ('What is yout favorite number? ')
+    print ('Interesting! I also love ' + favorite_number + '!')
 
 def get_name_and_zodiac_sign():
     """
@@ -29,8 +30,9 @@ def get_name_and_zodiac_sign():
     Then print out, "Interesting! My name is also X, and I'm also a Y!",
     where X and Y are replaced by the user's name and zodiac sign, respectively.
     """
-    # write your code here.
-
+    name = input('What is your name? ')
+    zodiac = input ('How old are you? ')
+    print('Interesting! My name is also ' + name + ", and I'm also a " + zodiac + '!')
 
 def get_name_and_age():
     """
@@ -39,4 +41,6 @@ def get_name_and_age():
     Then print out, "Interesting! My name is also X, and I'm also Y years old!",
     where X and Y are replaced by the user's name and age, respectively.
     """
-    # write your code here.
+    name = input('What is your name? ')
+    age = input ('How old are you? ')
+    print('Interesting! My name is also ' + name + ", and I'm also " + age + ' years old!')
